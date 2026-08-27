6173d40f2f984ac697ce8b1d147834a6ba11efc8
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-08-27 14:49:32 +0800

    update project type picker

--- ProjectTypePicker.cs
+++ ProjectTypePicker.cs
@@ -4,18 +4,26 @@ namespace inpsIDE
 {
     public partial class ProjectTypePicker : Form
     {
+        ClickableElement inpsGE = new ClickableElement("inpsGE");
+
         public ProjectTypePicker()
         {
             InitializeComponent();
 
-            projectTypeList.AddItem(new ClickableElement("inpsGE").SetEvent(() =>
-            {
-                MessageBox.Show("inpsGE project type selected");
-            }));
+            inpsGE.SetEvent(() => { inpsGE.Toggle(); });
+
+            projectTypeList.AddItem(inpsGE);
         }
 
         private void createButton_Click(object sender, EventArgs e)
         {
+            foreach (ClickableElement ce in projectTypeList.Controls)
+            {
+                if (ce.IsToggled())
+                {
+                    MessageBox.Show(ce.GetTitle());
+                }
+            }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
